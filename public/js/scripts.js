8b62a3c15287681cbbae0afb5d502eb3e126fd1e
yesseri <henrik.zenkert@gmail.com>
2021-01-10 00:05:20 +0100

    a LOT of css upgrades. Switched buttons to svg hands.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,39 +4,45 @@ const createGameButton = document.getElementById('createGameButton');
 const joinGameButton = document.getElementById('joinGameButton');
 const playAgainButton = document.getElementById('playAgainButton');
 const acceptInviteButton = document.getElementById('acceptInviteButton');
-const choiceButtons = document.getElementsByClassName("choiceButtons");
 const message = document.getElementById('message')
 
+const seeGameBox = () => {
+  const container = document.getElementById('container');
+  container.style.display = 'none';
+  const gameContainer = document.getElementById('gameContainer');
+  gameContainer.style.display = 'block';
+}
 
-let firstPlayer=false;
+let firstPlayer = false;
 let roomID;
-socket.on('newGame', (data) => {
-	roomID = data.roomID 
-	document.getElementById('roomName').innerHTML = roomID;
+socket.on('newGame', ({ roomID }) => {
+  document.getElementById('roomNameInfo').innerHTML = roomID;
 });
 
 createGameButton.addEventListener('click', function () {
-  firstPlayer=true;
+  const nameInput = document.getElementById("nameCreate").value;
+  firstPlayer = true;
+  message.display = "inline-block"
   message.innerText = 'Waiting for player two'
-	socket.emit('createGame', { name: 'Player One' });
+  socket.emit('createGame', { name: 'Player One' });
 });
 
 joinGameButton.addEventListener('click', function () {
-	const playerName = 'Player Two';
-	roomID = document.getElementById('joinRoomInput').value;
-	document.getElementById('roomNameInfo').innerHTML = "Room: " + roomID;
-	socket.emit('joinGame', { name: playerName, roomID });
+  const playerName = 'Player Two';
+  roomID = document.getElementById('joinRoomInput').value;
+  document.getElementById('roomNameInfo').innerHTML = "Room: " + roomID;
+  socket.emit('joinGame', { name: playerName, roomID });
 });
 
 playAgainButton.addEventListener('click', function () {
   message.innerText = 'Waiting for other player to accept. '
-	playAgainButton.style.display = 'none';
-	socket.emit('playAgain', { roomID });
+  playAgainButton.style.display = 'none';
+  socket.emit('playAgain', { roomID });
 })
 
 acceptInviteButton.addEventListener('click', function () {
-	acceptInviteButton.style.display = 'none';
-	socket.emit('acceptInvite', { roomID });
+  acceptInviteButton.style.display = 'none';
+  socket.emit('acceptInvite', { roomID });
 })
 socket.on('playAgainInvite', () => {
   playAgainButton.style.display = 'none';
@@ -52,20 +58,20 @@ socket.on('restartGame', () => {
 socket.on('player1Joined', (data) => {
   enableChoiceButtons()
   message.innerText = 'Game has started'
-	transition(data);
+  transition(data);
 });
 
 //Player 2 Joined
 socket.on('player2Joined', (data) => {
   enableChoiceButtons()
   message.innerText = 'New player joined, game has started'
-	transition(data);
+  transition(data);
 });
 
 socket.on('result', (data) => {
   console.log(data.winnerMessage)
-  message.innerText = data.winnerMessage 
-	playAgainButton.style.display = 'inline-block';
+  message.innerText = data.winnerMessage
+  playAgainButton.style.display = 'inline-block';
 });
 
 socket.on('opponentDisconnected', () => {
@@ -74,44 +80,47 @@ socket.on('opponentDisconnected', () => {
 })
 
 const resetGame = () => {
-	createGameButton.style.visibility = 'visible';
-	joinGameButton.style.visibility = 'visible';
+  createGameButton.style.visibility = 'visible';
+  joinGameButton.style.visibility = 'visible';
 }
 
 const transition = () => {
-	createGameButton.style.visibility = 'hidden';
-	joinGameButton.style.visibility = 'hidden';
+  createGameButton.style.visibility = 'hidden';
+  joinGameButton.style.visibility = 'hidden';
 }
 
-document.getElementById('rockButton').addEventListener('click', function () {
-  makeChoice('Rock')
-  disableChoiceButtons();
-});
-document.getElementById('paperButton').addEventListener('click', function () {
-  makeChoice('Paper')
-  disableChoiceButtons();
-});
-document.getElementById('scissorButton').addEventListener('click', function () {
-  makeChoice('Scissor')
-  disableChoiceButtons();
-});
+const choiceArray = ['Rock', 'Paper', 'Scissor']
+const choiceButtons = [];
 
+choiceArray.forEach((choice) => {
+  const button = document.getElementById(choice)
+  choiceButtons.push(button)
+  button.addEventListener('click', function () {
+    buttonClicked(choice)
+  });
+})
+
+const buttonClicked = (choice) => {
+  console.log(choice)
+  // makeChoice(choice)
+  // disableChoiceButtons();
+}
 
 const disableChoiceButtons = () => {
   for (button of choiceButtons) {
-	  button.disabled = true;
+    button.style.visibility == 'hidden'
   }
 }
 
 const enableChoiceButtons = () => {
   for (button of choiceButtons) {
-	  button.disabled = false;
+    button.disabled = false;
   }
 }
 
 const makeChoice = choice => {
   const choiceEvent = firstPlayer ? 'choicePlayerOne' : 'choicePlayerTwo'
-  socket.emit(choiceEvent,{
+  socket.emit(choiceEvent, {
     choice: choice,
     roomID: roomID
   });
