cca891099ae357478ff36ef28f9ee25ac715a108
YesSeri <henrik.zenkert@gmail.com>
2021-01-31 22:34:06 +0100

    Fixed minor css stuff

--- test.js
+++
@@ -1,8 +0,0 @@
-const roomName = 'aa43'
-let socket = {id: 'player1', size: 200}
-socket.roomInfo = {}
-socket.roomInfo[roomName] = {rules: 'aaa'}
-console.log(socket)
-const n = 'size'
-delete socket[n]
-console.log(socket)
